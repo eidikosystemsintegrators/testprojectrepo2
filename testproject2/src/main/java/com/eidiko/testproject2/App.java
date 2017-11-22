dc81f62edf78af3a8fd7ec1090e8540037307a9c
bandaru <bandaru@devopss.eidikointernal.com>
2017-11-22 14:56:54 -0500

    tpr1

---
+++ testproject2/src/main/java/com/eidiko/testproject2/App.java
@@ -0,0 +1,13 @@
+package com.eidiko.testproject2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
